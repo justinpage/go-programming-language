e33de798b4bdc2ce174b45546bb0e3fec88c2cd8
Justin Page <justin.page@motortrend.com>
2019-09-02 22:18:09 -0700

    Updated server1 and server2 example

--- 1-tutorial/7.server1.go
+++ 1-tutorial/7.server1.go
@@ -1,3 +1,4 @@
+// Server1 is a minimal "echo" server
 package main
 
 import (
@@ -8,9 +9,10 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// handler echoes the Path component of the request URL r
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
--- 1-tutorial/7.server2.go
+++ 1-tutorial/7.server2.go
@@ -1,3 +1,4 @@
+// Server2 is a minimal "echo" and counter server
 package main
 
 import (
@@ -13,9 +14,10 @@ var count int
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+// handler echoes the Path component of the request URL r
 func handler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
@@ -23,6 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
+// counter echoes the number of calls so far
 func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
