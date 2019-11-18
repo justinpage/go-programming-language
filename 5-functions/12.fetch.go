9ac12228a660821a329ffe24b0bb5af1ea424a3d
Justin Page <justin.page@motortrend.com>
2019-11-17 19:14:38 -0800

    Include defer examples and exercise

---
+++ 5-functions/12.fetch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		filename, n, err := fetch(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println("filename:", filename)
+		fmt.Println("written:", n)
+	}
+}
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create("/tmp/" + local)
+	if err != nil {
+		return "", 0, err
+	}
+
+	n, err = io.Copy(f, resp.Body)
+	// Close file, but prefer error from Copy, if any
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+
+	return local, n, err
+}
