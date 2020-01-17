0d355e45b76a8fa3cf666668428c55ee2f959f30
Justin Page <justin.page@motortrend.com>
2020-01-16 16:13:21 -0800

    Update exercise 8.1
    Clear terminal before showing next tick

--- 8-goroutines-and-channels/exercises/8.1.clockwall/clockwall.go
+++ 8-goroutines-and-channels/exercises/8.1.clockwall/clockwall.go
@@ -8,9 +8,11 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	go clear()
 	for _, arg := range os.Args[1:] {
 		z := strings.Split(arg, "=")
 		go dial(z[0], z[1])
@@ -31,5 +33,12 @@ func mustCopy(n string, src io.Reader) {
 	input := bufio.NewScanner(src)
 	for input.Scan() {
 		fmt.Printf("%-10s%8s\n", n, input.Text())
+	}
+}
+
+func clear() {
+	for {
+		time.Sleep(1 * time.Second)
+		print("\033[H\033[2J") // https://stackoverflow.com/a/22892171/2395590
 	}
 }
