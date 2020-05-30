c6d003dad28f60dcdcd526203e437ad9d88c1c5f
Justin Page <justin.page@motortrend.com>
2020-05-29 22:46:54 -0700

    Include finished looping in parallel example

---
+++ 8-goroutines-and-channels/examples/12.looping-in-parallel/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/justinpage/go-programming-language/8-goroutines-and-channels/examples/12.looping-in-parallel/thumbnail"
+)
+
+// makeThumbnails3 makes thumbnails of the specified files in parallel
+func makeThumbnails3(filenames []string) {
+	ch := make(chan struct{})
+	for _, f := range filenames {
+		go func(f string) {
+			thumbnail.ImageFile(f) // NOTE: ignoring errors
+			ch <- struct{}{}
+		}(f)
+	}
+
+	// Wait for goroutines to complete
+	for range filenames {
+		<-ch
+	}
+}
+
+// makeThumbnails4 makes thumbnails for the specified files in parallel.
+// It returns an error if any step failed.
+func makeThumbnails4(filenames []string) error {
+	errors := make(chan error)
+
+	for _, f := range filenames {
+		go func(f string) {
+			_, err := thumbnail.ImageFile(f)
+			errors <- err
+		}(f)
+	}
+	for range filenames {
+		if err := <-errors; err != nil {
+			return err // NOTE: incorrect: goroutine leak!
+		}
+	}
+
+	return nil
+}
+
+// makeThumbnails5 makes thumbnails for the specified files in parallel.
+// It returns the generated file names in an arbitrary order,
+// or an error if any step failed.
+func makeThumbnails5(filenames []string) (thumbfiles []string, err error) {
+	type item struct {
+		thumbfile string
+		err       error
+	}
+
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thumbfile, it.err = thumbnail.ImageFile(f)
+			ch <- it
+		}(f)
+	}
+
+	for range filenames {
+		it := <-ch
+		if it.err != nil {
+			return nil, it.err
+		}
+		thumbfiles = append(thumbfiles, it.thumbfile)
+	}
+
+	return thumbfiles, nil
+}
+
+// makeThumbnails6 makes thumbnails for each file received from the channel.
+// It returns the number of bytes occupied by the files it creates.
+//
+// The structure of the code below is a common and idiomatic pattern for looping
+// in parallel when we don't know the number of iterations.
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup // number of working goroutines
+	for f := range filenames {
+		wg.Add(1)
+		// worker
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb) // OK to ignore error
+			sizes <- info.Size()
+		}(f)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+	return total
+}
+
+func main() {
+	dir, _ := os.Getwd()
+	path := dir + "/images/"
+
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filenames := make(chan string)
+	go func() {
+		for _, file := range files {
+			filenames <- path + file.Name()
+		}
+		close(filenames)
+	}()
+
+	totalSize := makeThumbnails6(filenames)
+	log.Println(totalSize)
+}
